dc242ffb5ef5d58e9cae1497ba1e47ddc219f854
rkentmc <rkentmc@gmail.com>
2019-01-10 06:06:57 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+
+
+const courses = [
+    { id: 1, name: 'course1' },
+    { id: 2, name: 'course2' },
+    { id: 3, name: 'course3' }
+]
+
+app.get('/', (req, res) => {
+
+   res.send('Hello World!!!')
+
+});
+
+app.get('/api/courses', (req, res) => {
+
+    res.send([1, 2, 3])
+ 
+ });
+
+ app.get('/api/courses/:id', (req, res) => {
+
+    const course = course.find(c => c.id === parseInt(req.params.id));
+    if (! course) res.status(404).send('Course was not found') 
+    res.send(course);
+ });
+
+
+const port = process.env.PORT || 3000
+app.listen(port, () => 
+    console.log('Listening on port `${port}`')
+);
